656414e19a28c45e5e228fa81e94c571781d4c17
Sushil Agale <sushil.agale@gmail.com>
2025-07-14 23:05:17 +0530

    ASSIGNMENT 1: Module 2: Basic Python Concepts
    ASSIGNMENT 1:
    Module 2: Basic Python Concepts

---
+++ task1.py
@@ -0,0 +1,14 @@
+# Calculator program
+
+a = int(input("Enter frist no: "))
+b= int(input("Enter second no: "))
+
+sum = a + b;
+sub = a - b;
+mul = a * b;
+div = a / b;
+
+print("The sum is:",sum)
+print("The sub is:",sub)
+print("The mul is:",mul)
+print("The division is:",div)
